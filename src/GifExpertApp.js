5bece566019b7ba0aa99f79ab348d4bd7e634564
Xavier <xavier.coello@epn.edu.ec>
2021-11-06 01:01:37 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+
+
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    // const handleAdd = () => {
+    //     setCategories([...categories, 'Ranma 1/2']);
+    // }
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr />
+
+            <ol>
+                {
+                    categories.map( category => 
+                        <GifGrid
+                            key={ category } 
+                            category={ category }    
+                        />
+                    )
+                }
+            </ol>            
+        </>
+    );
+}
+
+
+export default GifExpertApp
+
